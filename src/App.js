40e83fa97a940d8022556585483cb0f21edf2d86
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2019-12-12 12:03:47 +0100

    Last version of website

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ import IconLoader from './components/IconLoader'
 function App() {
 
   const widgets = [
-    {image_url: "https://static.boutique.orange.fr/media-cms/mediatheque/636x900-iphone-7-noir---vue-1-137069.jpg"},
+    {rotation: 30, image_url: "https://static.boutique.orange.fr/media-cms/mediatheque/636x900-iphone-7-noir---vue-1-137069.jpg"},
     {image_url: "https://images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png"},
+    {rotation: 136, image_url: "https://images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png"},
+    {image_url: "https://www.zdnet.fr/images-produits/edit/2019/09/iphone-11-pro-the-core-1-big.jpg"},
+    {image_url: "https://images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png"},
+    {image_url: "https://www.zdnet.fr/images-produits/edit/2019/09/iphone-11-pro-the-core-1-big.jpg"},
+    {image_url: "https://static.boutique.orange.fr/media-cms/mediatheque/636x900-iphone-7-noir---vue-1-137069.jpg"},
+    {image_url: "https://www.zdnet.fr/images-produits/edit/2019/09/iphone-11-pro-the-core-1-big.jpg"},
     {image_url: "https://images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png"},
   ]
 
