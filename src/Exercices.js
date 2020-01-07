af3c5aecf840a471a1f67b997aa778889a959c40
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-07 15:13:10 +0100

    Added Landing Page

--- src/Exercices.js
+++ src/Exercices.js
@@ -1,59 +1,11 @@
 import React from 'react'
 
-function Mapper({pokemons}){
+function LandingPage(props){
     return(
-        <div>
-            <h1>MAPPER</h1>
-            {pokemons.map(poke => <p>{poke.name}</p>)}
+        <div className="flex-center totalh">
+            <h1 className="animated tada infinite">MA PAGE</h1>
         </div>
     )
 }
 
-function Exercices(e_props){
-
-    const tableau = [
-        {name: "Bulbizarre", type: "Plante"},
-        {name: "Salamèche", type: "Feu"},
-        {name: "Carapuce", type: "Eau"},
-        {name: "Pikachu", type: "Electricité"}
-    ]
-
-    const types = tableau.map(pokemon => pokemon.type)
-    console.log('types', types)
-
-    return(
-        <div>
-            <div>EXERCICES</div>
-            <Mapper 
-                pokemons={tableau}
-            />
-        </div>
-    )
-}
-
-function Consumer({size, adress}){
-    return(
-        <div>
-            <h2>Consommateur</h2>
-            <p>{size}</p>
-            <p>{adress}</p>
-        </div>
-    )
-}
-
-function Deconstructor(props){
-    return(
-        <div>
-            <h1>Déconstructeur</h1>
-            <p>{props.age}</p>
-            <Consumer {...props} />
-            <Consumer 
-                size={122}
-                adress="2 rue de la laque"
-            />
-
-        </div>
-    )
-}
-
-export default Exercices
+export default LandingPage
