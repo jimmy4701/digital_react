8741435d31e3482211fab59232732f3be06660da
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-08 15:21:25 +0100

    Prepared tchat app frame

--- src/Exercices.js
+++ src/Exercices.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import SigninForm from './components/SigninForm'
 
 function LandingPage(props){
+    const [signed, setSigned] = useState(false)
+
+    const sign_in = (email, password) => {
+        setSigned(true)
+    }
+
+    console.log('MODIF PAGE')
+
     return(
-        <div className="flex-center totalh">
-            <h1 className="animated tada infinite">MA PAGE</h1>
+        <div className="main-container">
+            <div id="left-part">
+                <p>MES AMIS</p>
+            </div>
+            <div id="right-part">
+                <p>DISCUSSION</p>
+            </div>
         </div>
     )
 }
